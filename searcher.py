619b70caa9fd63ee54fa66fdb2268c1a6ad6f2d7
Edgar <hi.ed@hotmail.com>
2017-09-20 21:59:00 -0500

    Arreglar el filtro

--- searcher.py
+++ searcher.py
@@ -9,14 +9,12 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-'''
 data = api.search(q='@comoayudarmx',
         result_type="recent")
 
 print("!!!!TODA DATA!!!!")
 print(data[1])
 print("!!!TODA DATA!!")
-'''
 tuits = []
 
 
